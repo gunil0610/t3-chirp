741d46e91c2848289b58f4151e9e518e2d7655d1
gunil0610 <gunil.na1@gmail.com>
2023-08-30 18:37:11 +0900

    wip: basic trpc test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.example.getAll.useQuery();
 
   return (
     <>
