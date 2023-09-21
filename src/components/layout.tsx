71d7e8261f801b45d29e60ba0b28f10ad5a9a84e
gunil0610 <gunil.na1@gmail.com>
2023-09-22 05:39:17 +0900

    feat: use scroll area for better scroll style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,14 @@
 import { type PropsWithChildren } from "react";
+import { ScrollArea } from "./ui/scroll-area";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
-      <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl ">
-        {props.children}
-      </div>
+      <ScrollArea className="h-screen w-full md:max-w-2xl ">
+        <div className="h-full w-full border-x border-slate-400 ">
+          {props.children}
+        </div>
+      </ScrollArea>
     </main>
   );
 };
